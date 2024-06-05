53da2b455054f7685e72dfe8b68149ec4ceb963b
Hemvyas <hem.vyas09@gmail.com>
2024-06-06 01:53:08 +0530

    handled error and loading

--- assignment/src/ProductList.js
+++ assignment/src/ProductList.js
@@ -14,21 +14,33 @@ import {
   Typography,
   useMediaQuery,
   useTheme,
+  Alert,
+  Snackbar,
+  CircularProgress,
 } from "@mui/material";
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [loading,setLoading]=useState(false);
+  const [error,setError]=useState(null)
 
   const theme = useTheme();
   const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
   useEffect(() => {
     const getProducts = async () => {
-      const data = await fetchProducts();
-      setProducts(data);
-      setFilteredProducts(data);
+      try {
+       setLoading(true);
+       const data = await fetchProducts();
+       setProducts(data);
+       setFilteredProducts(data); 
+       setLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
     };
     getProducts();
   }, []);
@@ -40,6 +52,10 @@ const ProductList = () => {
       )
     );
   }, [searchTerm, products]);
+
+    const handleSnackbarClose = () => {
+      setError(null);
+    };
 
   return (
     <Container
@@ -59,48 +75,66 @@ const ProductList = () => {
         onChange={(e) => setSearchTerm(e.target.value)}
         style={{ marginBottom: theme.spacing(2) }}
       />
-      <TableContainer
-        component={Paper}
-        style={{
-          marginTop: theme.spacing(2),
-          border: `1px solid ${theme.palette.divider}`,
-          borderRadius: "8px",
-        }}
-      >
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Image</TableCell>
-              <TableCell>Title</TableCell>
-              {!isSmallScreen && <TableCell>Description</TableCell>}
-              <TableCell>Price</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {filteredProducts.map((product) => (
-              <TableRow key={product.id}>
-                <TableCell style={{ padding: theme.spacing(1) }}>
-                  <img
-                    src={product.image}
-                    alt={product.title}
-                    style={{
-                      width: isSmallScreen ? 30 : 50,
-                      height: isSmallScreen ? 30 : 50,
-                    }}
-                  />
-                </TableCell>
-                <TableCell style={{ padding: theme.spacing(1) }}>
-                  {product.title}
-                </TableCell>
-                {!isSmallScreen && <TableCell>{product.description}</TableCell>}
-                <TableCell style={{ padding: theme.spacing(1) }}>
-                  ${product.price}
-                </TableCell>
+      {loading ? (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <CircularProgress />
+        </div>
+      ) : error ? (
+        <Snackbar
+          open={Boolean(error)}
+          autoHideDuration={6000}
+          onClose={handleSnackbarClose}
+        >
+          <Alert onClose={handleSnackbarClose} severity="error">
+            {error}
+          </Alert>
+        </Snackbar>
+      ) : (
+        <TableContainer
+          component={Paper}
+          style={{
+            marginTop: theme.spacing(2),
+            border: `1px solid ${theme.palette.divider}`,
+            borderRadius: "8px",
+          }}
+        >
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Image</TableCell>
+                <TableCell>Title</TableCell>
+                {!isSmallScreen && <TableCell>Description</TableCell>}
+                <TableCell>Price</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {filteredProducts.map((product) => (
+                <TableRow key={product.id}>
+                  <TableCell style={{ padding: theme.spacing(1) }}>
+                    <img
+                      src={product.image}
+                      alt={product.title}
+                      style={{
+                        width: isSmallScreen ? 30 : 50,
+                        height: isSmallScreen ? 30 : 50,
+                      }}
+                    />
+                  </TableCell>
+                  <TableCell style={{ padding: theme.spacing(1) }}>
+                    {product.title}
+                  </TableCell>
+                  {!isSmallScreen && (
+                    <TableCell>{product.description}</TableCell>
+                  )}
+                  <TableCell style={{ padding: theme.spacing(1) }}>
+                    ${product.price}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
     </Container>
   );
 };
